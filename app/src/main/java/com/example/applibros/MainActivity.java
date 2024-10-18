d82f6e52ef6b3ff9cdf6716cabcd9a5ec62467a6
Davinson Granada <118221608+Davinsongranada@users.noreply.github.com>
2024-10-18 09:55:55 -0500

    Cambio 4

--- app/src/main/java/com/example/applibros/MainActivity.java
+++ app/src/main/java/com/example/applibros/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.applibros;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText fullName, userName, password;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fullName = findViewById(R.id.etFullName);
+        userName = findViewById(R.id.etUserName);
+        password = findViewById(R.id.etPassword);
+        btnSave = findViewById(R.id.idSave);
+
     }
 }
